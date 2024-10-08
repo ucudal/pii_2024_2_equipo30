8a4d93e2e5916b633a2a72e0271537c84c26b979
Ale Hernandez <alehernandez@gmail.com>
2024-10-08 20:59:05 -0300

    Carcasa de ataque echo(segun el UML)

---
+++ src/Library/Ataque.cs
@@ -0,0 +1,11 @@
+﻿namespace Library;
+
+public class Ataque
+{
+    public string nombre { get; set; }//toma el nombre del pokemon
+    public int Daño { get; set; }//toma el valor de daño para calcular cuanto daño va a haceer
+    public int atacar(int Daño, string Nombre)//clase
+    {
+        return 1;//ejemplo
+    }
+}
