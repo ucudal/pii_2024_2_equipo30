f0affbe1b4ea4cdd2711802d8dfdaf294d765706
Mauricio Segovia <mauricio.segovia@correo.ucu.edu.uy>
2024-10-08 20:54:00 -0300

    Agregada clase Pokemon, falta terminarla

---
+++ src/Library/Pokemon.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Library;
+
+public class Pokemon
+{
+    public string Nombre;
+    public string Tipo;
+    public int Vida;
+    private List<Estados> EstadosList;
+    public List<Resistencias> ResistenciasList;
+
+    public Pokemon(string nombre)
+    {
+        string Nombre = nombre;
+        string Tipo = this.Tipo;
+        int Vida = this.Vida;
+        EstadosList = new List<Estados>();
+    }
+
+    public void AtaqueNormal(Ataque ataque)
+    {
+
+    }
+
+    public void AtaqueEspecial(AtaqueEspecial ataque)
+    {
+        
+    }
+
+
+
+}
