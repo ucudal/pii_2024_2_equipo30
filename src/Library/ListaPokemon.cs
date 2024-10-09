6acb0ea92aaf3cf540964d754e930d2e52733964
Gaston Baranov Rodriguez <GASTON.BARANOV@correo.ucu.edu.uy>
2024-10-09 17:39:10 -0300

    Tipos Pokemon antes API

--- src/Library/ListaPokemon.cs
+++ src/Library/ListaPokemon.cs
@@ -11,21 +11,21 @@ public class ListaPokemon
     {
         ListaDePokemons = new List<Pokemon>
         {
-            new Pokemon("Pikachu", 100, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Charizard", 120, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Blastoise", 110, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Venusaur", 110, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Gengar", 90, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Snorlax", 160, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Machamp", 130, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Alakazam", 95, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Jolteon", 80, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Lapras", 120, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Gyarados", 125, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Dragonite", 150, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Arcanine", 115, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Umbreon", 95, movimientos.ObtenerMovimientosAleatorios()),
-            new Pokemon("Tyranitar", 140, movimientos.ObtenerMovimientosAleatorios())
+            new Pokemon("Pikachu", 100, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Electrico),
+            new Pokemon("Charizard", 120, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Fuego),
+            new Pokemon("Blastoise", 110, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Agua),
+            new Pokemon("Venusaur", 110, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Planta),
+            new Pokemon("Gengar", 90, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Fantasma),
+            new Pokemon("Snorlax", 160, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Normal),
+            new Pokemon("Machamp", 130, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Lucha),
+            new Pokemon("Alakazam", 95, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Psiquico),
+            new Pokemon("Jolteon", 80, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Electrico),
+            new Pokemon("Lapras", 120, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Hielo),
+            new Pokemon("Gyarados", 125, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Volador),
+            new Pokemon("Dragonite", 150, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Dragón),
+            new Pokemon("Arcanine", 115, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Fuego),
+            new Pokemon("Umbreon", 95, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Oscuro),
+            new Pokemon("Tyranitar", 140, movimientos.ObtenerMovimientosAleatorios(), TipoPokemon.Roca)
         };
     }
 
