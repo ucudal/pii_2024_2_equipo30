d837fd28555f379a895788aba417229a26643ee9
Ale Hernandez <alehernandez@gmail.com>
2024-10-09 11:42:39 -0300

    Cambio de turno creado(siguiendo UML)

---
+++ src/Library/CambioPokemon.cs
@@ -0,0 +1,12 @@
+﻿namespace Library;
+
+public class CambioPokemon
+{
+    public (List<Pokemon>) Nombres { get; set;}//lista de nombre de los pokemons,esta de ejemplo "pokemon" pero se usaria la lista de pokemons del entrenador
+    public CambioDeTruno CambioDeTruno { get; set; }//cambia de turno
+
+    public string CambiarPokemon(bool CambioDeTruno, string Nombres)
+    {
+        CambioDeTruno = true;
+    }
+}
