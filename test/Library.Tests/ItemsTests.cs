0cd10303e22c59f7e05cda102323c59fa645a9a7
Ale Hernandez <alehernandez@gmail.com>
2024-11-07 18:04:18 -0300

    obsoleto

--- test/Library.Tests/ItemsTests.cs
+++ test/Library.Tests/ItemsTests.cs
@@ -5,21 +5,29 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 using Library;
-
-
+using NUnit.Framework;
 namespace Library.Tests;
+
 public class ItemsTests
 {
+    private Pokemon _pokemon;
+    private SuperPotion _superPotion;
+    private TotalCure _totalCure;
+    private Revive _revive;
+    
     [SetUp]
     public void Setup()
     {
-    }
-    
+        _pokemon = new Pokemon("Pikachu", 1, 100, 55, 40, 50, 50, new Type(), new List<Move>());
+        _pokemon.VidaMax = 100;
+    }  
+
     [Test]
     public void SuperPotion_Heal()
     {
         
         
+        //Asserts
         
     }
     
@@ -27,11 +35,18 @@ public class ItemsTests
     public void SuperPotion_dontHeal()
     {
         
+        
+        //Asserts
+        
+        
+        
     }
     
     [Test]
     public void TotalCure_Cleanstates()
     {
+        
+        //Asserts
         
     }
     
@@ -39,18 +54,28 @@ public class ItemsTests
     public void TotalCure_dontcleanstates()
     {
         
+        
+        //Asserts
+        
+        
     }
     
     [Test] 
     public void Revive_CanRevive()
     {
+        
+        
+        //Asserts
+        
             
     }
      
     [Test] 
     public void Revive_Leavedead()
     {
-            
+        
+        //Asserts
+        
     }
     
 }
