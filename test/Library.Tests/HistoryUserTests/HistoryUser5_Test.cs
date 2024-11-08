e0526e6623edf334b73fdd186d81a6f78befee29
Mauricio Segovia <mauricio.segovia@correo.ucu.edu.uy>
2024-11-08 18:29:27 -0300

    Modificados los test de HistoryUser3 y HistoryUser5

--- test/Library.Tests/HistoryUserTests/HistoryUser5_Test.cs
+++ test/Library.Tests/HistoryUserTests/HistoryUser5_Test.cs
@@ -1,20 +1,38 @@
 ﻿namespace Library.Tests.HistoryUserTests;
 
+using Library;
+using NUnit.Framework;
+using System.Collections.Generic;
 public class HistoryUser5_Test
-{
-    [SetUp]
-    public void Setup()
-    {
-        
-    }
 
+[TestFixture]
+public class PokemonTests
+{
     [Test]
-    public void HistoryUser5_Test1()
+    public void Test_IndicadorDeTurno_CorrectamenteMuestraElTurnoActual()
     {
-        //arrange
+        // Arrange: Crear los Pokémon con vida inicial
+        Pokemon1 = new Pokemon("Charizard", 6, 100, 84, 78, 109, 85, fireType, new List<Move>());
+        Pokemon2 = new Pokemon("Venusaur", 3, 100, 82, 83, 100, 100, grassType, new List<Move>());
+
+        Jugador jugador1 = new Jugador("Jugador 1", listPokemonJugador1);
+        Jugador jugador2 = new Jugador("Jugador 2", listPokemonJugador2);
+        // Creamos el juego con los dos Pokémon
         
-        //act
+        // Act: El jugador realiza una acción (por ejemplo, un ataque)
+        juego.RealizarTurno(pokemonJugador, pokemonOponente); // Pikachu ataca a Charizard
         
-        //assert
+        // Assert: Verificar que el indicador de turno se actualice correctamente
+        string turnoActual = juego.ObtenerIndicadorTurno();
+        
+        // Comprobamos que el indicador de turno se ha actualizado correctamente
+        Assert.AreEqual("Es el turno de Charizard", turnoActual); // Después del turno de Pikachu, debería ser el turno de Charizard
+        
+        // El jugador oponente realiza su acción
+        juego.RealizarTurno(pokemonOponente, pokemonJugador); // Charizard ataca a Pikachu
+        
+        // Verificar nuevamente el indicador de turno
+        turnoActual = juego.ObtenerIndicadorTurno();
+        Assert.AreEqual("Es el turno de Pikachu", turnoActual); // Después del turno de Charizard, debería ser el turno de Pikachu
     }
 }
